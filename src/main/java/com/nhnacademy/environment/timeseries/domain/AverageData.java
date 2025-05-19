15f144e398b555afe6ce5f40455f63eef618daf6
ATGN02-018 <ygw1026@naver.com>
2025-05-19 13:14:37 +0900

    local docker 환경에서 테스트

--- src/main/java/com/nhnacademy/environment/timeseries/domain/AverageData.java
+++ src/main/java/com/nhnacademy/environment/timeseries/domain/AverageData.java
@@ -3,6 +3,7 @@ package com.nhnacademy.environment.timeseries.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
@@ -92,4 +93,9 @@ public class AverageData {
      */
     @Column(name = "created_at", insertable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
