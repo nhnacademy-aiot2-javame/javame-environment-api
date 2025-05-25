9b31dbed25c18aa298c9e05118f35bf94e7a58b6
jangwonseong <jangwonsung@gmail.com>
2025-05-25 23:31:48 +0900

    feat-environment-local #30 : 버텍스 AI 제거, Spring AI에서 Gemini AI로 변경 및 리팩토링, 서비스 및 컨트롤러 구현

--- src/main/java/com/nhnacademy/environment/report/dto/ReportRequestDto.java
+++ src/main/java/com/nhnacademy/environment/report/dto/ReportRequestDto.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.environment.report.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReportRequestDto {
 
     private String reportType;
