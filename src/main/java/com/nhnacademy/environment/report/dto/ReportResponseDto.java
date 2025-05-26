71b666429d3a7272477b6a4616dd13afaaec0028
jangwonseong <jangwonsung@gmail.com>
2025-05-26 21:30:51 +0900

    feat-environment-local #30 : cpu온도에 대한 값만 하드코딩해서 제미나이가 분석이 되고 그래프가 그려지는지를 확인

--- src/main/java/com/nhnacademy/environment/report/dto/ReportResponseDto.java
+++ src/main/java/com/nhnacademy/environment/report/dto/ReportResponseDto.java
@@ -18,11 +18,11 @@ public class ReportResponseDto {
 
     private List<ChartDataDto> chartVisualizations;
 
-    /**
-     * 표 형태로 보여줄 상세 데이터 리스트 (선택적)
-     * 각 Map은 테이블 행을 나타내며, Key는 컬럼명, Value는 셀 값
-     */
-    private List<Map<String, Object>> detailedMetricsTable;
+//    /**
+//     * 표 형태로 보여줄 상세 데이터 리스트 (선택적)
+//     * 각 Map은 테이블 행을 나타내며, Key는 컬럼명, Value는 셀 값
+//     */
+//    private List<Map<String, Object>> detailedMetricsTable;
 
     private String reportOverallTitle;
 
